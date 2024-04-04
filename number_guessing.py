78a28aa446670d57afcb86ef00e9b2e7d2db81ef
AVIGYARAJ46 <156205675+AVIGYARAJ46@users.noreply.github.com>
2024-04-05 01:16:15 +0530

    Add files via upload

---
+++ number_guessing.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  5 01:13:48 2024
+
+@author: avigy
+"""
+
+import random
+
+def getRandomNumber():
+    return random.randrange(1, 100)
+
+def giveHint(number, guess):
+    if guess > (number + 10) or guess < (number - 10):
+        return "Cold"
+    elif number == guess:
+        return "Right"
+    else:
+        return "Hot"
+
+
+def runGuess():
+    secretNumber = getRandomNumber()
+    
+    while True:
+        user_guess = int(input("Enter a number between 1 and 100: "))
+        hint = giveHint(secretNumber, user_guess)
+        if hint == "Right":
+            print("You guessed it Right!")
+            break
+        else:
+            print(hint)
+            
+if __name__ == '__main__':
+    runGuess()
+    secretNumber = getRandomNumber()
+    
+    while True:
+        user_guess = int(input("Enter a number between 1 and 100: "))
+        hint = giveHint(secretNumber, user_guess)
+        if hint == "Right":
+            print("You guessed it Right!")
+            
+        else:
+            print(hint)
+            
+if __name__ == '__main__':
+    runGuess()
